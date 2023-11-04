44ccb658b28a367342c35a1a1af4efd3f25e90e9
Harshath <harshathmkulal@gmail.com>
2023-11-04 13:02:56 +0530

    Update Login.jsx

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:3001/login", { email: email, password: password })
+      .post("https://login-register-kappa.vercel.app/login", { email: email, password: password })
       .then((result) => {
         //console.log(result);
         if (result.data === "success") {
